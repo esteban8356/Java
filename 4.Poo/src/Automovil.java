c7ade6618125dcdc385d9291eb6bc56291e4d35c
esteban8356 <esteban8356m@gmail.com>
2024-12-15 12:30:41 -0500

    edit de poo

--- 4.Poo/src/Automovil.java
+++ 4.Poo/src/Automovil.java
@@ -1,9 +1,41 @@
 public class Automovil {
 
-    String fabricante;
-    String modelo;
-    String color = "gris";
-    double cilindrada;
+    private String fabricante;
+    private String modelo;
+    private String color = "gris";
+    private double cilindrada;
+    private int capacidadTanque = 40;
+
+    public Automovil() {
+    }
+
+    // constructor
+
+    public Automovil(String fabricante, String modelo) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public Automovil(String fabricante, String modelo, String color) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil(String fabricante, String modelo, String color, double cilindrada, int capacidadTanque) {
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+        this.cilindrada = cilindrada;
+        this.capacidadTanque = capacidadTanque;
+    }
 
     public String verDetalle() {
 
@@ -19,6 +51,54 @@ public class Automovil {
 
     public String frenar() {
         return "auto " + this.fabricante + " frenando";
+    }
+
+    public float calcularConsumo(int km,float porcentajeGasolina){
+        return km / (capacidadTanque * porcentajeGasolina);
+    }
+
+    public float calcularConsumo(int km,int porcentajeGasolina){
+        return km / (capacidadTanque * (porcentajeGasolina/100f));
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(double cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCapacidadTanque() {
+        return capacidadTanque;
+    }
+
+    public void setCapacidadTanque(int capacidadTanque) {
+        this.capacidadTanque = capacidadTanque;
     }
 
 }
