b1edaaa6c75fcab48dd5c877739cef9d20a60ed1
esteban8356 <esteban8356m@gmail.com>
2025-01-05 22:25:01 -0500

    empezamos a dibujar el html

---
+++ 8.POO_Clases_Abstractas/src/org/esteban/pooclasesabstractas/form/elementos/TexareaForm.java
@@ -0,0 +1,43 @@
+package org.esteban.pooclasesabstractas.form.elementos;
+
+public class TexareaForm extends ElementoForm {
+
+    private int filas;
+    private int columnas;
+
+    public TexareaForm(String nombre) {
+        super(nombre);
+    }
+
+    public TexareaForm(String nombre, int filas, int columnas) {
+        super(nombre);
+        this.filas = filas;
+        this.columnas = columnas;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public int getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+
+    @Override
+    public String dibujarHtml() {
+        return "<Textara name='"
+                + this.nombre +"'  cols='"
+                + this.columnas + "'  rows='"
+                + this.filas + "' >"
+                + this.valor + "</Textara>";
+
+    }
+}
