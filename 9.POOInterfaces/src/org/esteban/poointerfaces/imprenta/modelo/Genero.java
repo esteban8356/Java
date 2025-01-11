70f2434defee25ae2844c3daf1aedbee93e60ee9
esteban8356 <esteban8356m@gmail.com>
2025-01-10 22:42:31 -0500

    interfaces

--- 9.POOInterfaces/src/org/esteban/poointerfaces/imprenta/modelo/Genero.java
+++ 9.POOInterfaces/src/org/esteban/poointerfaces/imprenta/modelo/Genero.java
@@ -1,4 +1,11 @@
 package org.esteban.poointerfaces.imprenta.modelo;
 
 public enum Genero {
+    DRAMA,
+    ACCION,
+    AVENTURA,
+    TERROR,
+    CIENCIA_FICCION,
+    PROGRAMACION
+
 }
--- 9.POOInterfaces/src/org/esteban/poointerfaces/imprenta/modelo/Informe.java
+++ 9.POOInterfaces/src/org/esteban/poointerfaces/imprenta/modelo/Informe.java
@@ -1,4 +1,19 @@
 package org.esteban.poointerfaces.imprenta.modelo;
 
-public class Informe {
+public class Informe extends Hoja implements Imprimible{
+
+    private String autor;
+    private String revisor;
+
+    public Informe(String autor ,String revisor,String contenido) {
+        super(contenido);
+        this.autor = autor;
+        this.revisor = revisor;
+    }
+    @Override
+    public String imprimir() {
+        return "Intorme escrito por " + autor
+                +" Revisado por " + revisor
+                +"\n" + this.contenido;
+    }
 }
