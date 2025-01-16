7cdf9ca8c7effd2c5b2682d50f941d60d5daf352
esteban8356 <esteban8356m@gmail.com>
2025-01-16 14:42:09 -0500

    genericos

--- 10.Poo_interfaces_repositorio/src/org/esteban/generics/ElemploGenericos.java
+++ 10.Poo_interfaces_repositorio/src/org/esteban/generics/ElemploGenericos.java
@@ -3,14 +3,26 @@ package org.esteban.generics;
 import org.esteban.poointerfaces.modelo.Cliente;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ElemploGenericos {
     public static void main(String[] args) {
 
-        List clientes = new ArrayList();
+        List<Cliente> clientes = new ArrayList<>();
         clientes.add(new Cliente("Andres","Guzman"));
 
-        Cliente andres = (Cliente) clientes.iterator().next();
+        Cliente andres = clientes.iterator().next();
+        Cliente[] clientesarreglo = {new Cliente("luci","martinez"),
+        new Cliente("esteban","munoz")};
+        Integer[] enterosarreglo = {1,2,3};
+
+        List<Cliente> clienteslista = formArrayToList(clientesarreglo);
+
+        clienteslista.forEach(System.out::println);
+    }
+
+    public static List<Cliente> formArrayToList(Cliente[] c) {
+        return Arrays.asList(c);
     }
 }
