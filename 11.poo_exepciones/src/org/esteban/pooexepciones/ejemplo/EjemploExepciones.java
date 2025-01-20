526686ccdf87272244fc8d5050402f245eb210a7
esteban8356 <esteban8356m@gmail.com>
2025-01-20 18:28:06 -0500

    manejo de exepciones

--- 11.poo_exepciones/src/org/esteban/pooexepciones/ejemplo/EjemploExepciones.java
+++ 11.poo_exepciones/src/org/esteban/pooexepciones/ejemplo/EjemploExepciones.java
@@ -9,21 +9,11 @@ public class EjemploExepciones {
         String numerador = JOptionPane.showInputDialog("Ingrese un entero numerador");
         String denominador = JOptionPane.showInputDialog("Ingrese un entero denominador");
 
-        int divisor;
-        double division;
-
         try {
-           // divisor = Integer.parseInt(valor);
-            //division = cal.dividir(10,divisor);
-            //System.out.println(division);
-
             double divicion2 = cal.dividir(numerador,denominador);
             System.out.println("divicion2 = " + divicion2);
         }
-        catch (NumberFormatException nfe) {
-            System.out.println("se detecto una exepcion porfavor ingrese nuevamente el valor numerico " + nfe.getMessage());
-            main(args);
-        }catch (FormatoNumeroException e){
+        catch (FormatoNumeroException e){
             System.out.println("Se detecto una exepcion: ingrese un numero valido : " + e.getMessage());
             e.printStackTrace(System.out);
         }
