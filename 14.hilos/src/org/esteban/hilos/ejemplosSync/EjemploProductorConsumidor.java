c5159ea5c8a00909b7b3817c784f450ff588f230
esteban8356 <esteban8356m@gmail.com>
2025-02-02 23:13:46 -0500

    ejemplo hilos

---
+++ 14.hilos/src/org/esteban/hilos/ejemplosSync/EjemploProductorConsumidor.java
@@ -0,0 +1,9 @@
+package org.esteban.hilos.ejemplosSync;
+
+public class EjemploProductorConsumidor {
+    public static void main(String[] args) {
+        Panaderia p = new Panaderia();
+        new Thread(new Panadero(p)).start();
+        new Thread(new Consumidor(p)).start();
+    }
+}
